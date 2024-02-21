6ee517f34c05fd6af3bbb5c7617a03369866e9cc
tatoo40 <jfernandezberrutti@gmail.com>
2024-02-20 21:18:05 -0300

    cambios hasta el https

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,26 @@ import { AppModule } from './app.module';
 import { PrismaClientExceptionFilter } from './prisma-client-exception.filter';
 import * as session from 'express-session'
 import * as passport from 'passport'
-
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+
+  
+  const httpsOptions = {
+    key: fs.readFileSync('./src/cert/key.pem'),
+    cert: fs.readFileSync('./src/cert/cert.pem'),
+  };
+
+
+  const app = await NestFactory.create(AppModule,{ httpsOptions });
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+
+  
 
   app.use(
     session({
